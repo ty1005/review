4e809ee0cd93a0a89f6e725c7266072064256393
ty1005 <tzuyin1005@gmail.com>
2026-04-27 16:59:10 +0800

    Add some memo

--- review.py
+++ review.py
@@ -5,11 +5,11 @@ with open('reviews.txt', 'r') as f :
         data.append(line)
         count += 1 # count = count + 1
         if count % 10000 == 0: #求餘數
-            print(len(data))
+            print(len(data)) #data是一個清單，計算data裡有幾個物件
 print('Done! Totally', len(data), 'reviews')
 
 sum_len = 0
-for d in data:
-    sum_len = sum_len + len(d)
+for d in data: #d是個字串
+    sum_len += len(d)
 
 print ('留言平均', sum_len/ len(data), '字')
